870ac0a07c842d04937c8a824659c8bb81aabcc3
VIKTOR CHIARCOS <viktor.chiarcos@gmail.com>
2026-01-28 12:11:34 +0000

    Improve install script by checking for ~/bin directory
    Added a check to see if ~/bin exists before creating it.

--- vpackage/git-send/install.sh
+++ vpackage/git-send/install.sh
@@ -3,9 +3,10 @@
 printf "Klicke ENTER um die Installation zu starten oder drücke Strg/Control+C zum abbrechen"
 read
 if [ -e ~/bin ]
-  else mkdir ~/bin ; cd ~/bin
+  then echo "~/bin Ordner muss nicht erstellt werden, da er bereits exestiert"
+  else mkdir ~/bin
 fi
-
+cd ~/bin
 echo `curl -L https://raw.githubusercontent.com/viktor-chiarcos/Programms/refs/heads/main/git-send.sh` > git-send
 chmod a+x git-send
 echo "Das Programm ist heruntergeladen"
