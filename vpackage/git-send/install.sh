082a10d8844ad10cec4794253e83157a112488cc
VIKTOR CHIARCOS <viktor.chiarcos@gmail.com>
2026-01-28 12:19:39 +0000

    Fix installation script for git-send

--- vpackage/git-send/install.sh
+++ vpackage/git-send/install.sh
@@ -3,8 +3,8 @@
 printf "Klicke ENTER um die Installation zu starten oder drücke Strg/Control+C zum abbrechen"
 read
 if [ -e ~/bin ]
-  then echo "~/bin Ordner muss nicht erstellt werden, da er bereits exestiert"
-  else mkdir ~/bin
+echo "~/bin Ordner muss nicht erstellt werden, da er bereits exestiert"
+else mkdir ~/bin
 fi
 cd ~/bin
 echo `curl -L https://raw.githubusercontent.com/viktor-chiarcos/Programms/refs/heads/main/git-send.sh` > git-send
