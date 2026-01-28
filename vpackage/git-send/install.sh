62750aa7fb05824c27bced0dff0b650d83251095
VIKTOR CHIARCOS <viktor.chiarcos@gmail.com>
2026-01-28 12:35:39 +0000

    Simplify installation script by removing prompt
    Remove user prompt and read command before installation.

--- vpackage/git-send/install.sh
+++ vpackage/git-send/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-printf "Klicke ENTER um die Installation zu starten oder drücke Strg/Control+C zum abbrechen"
-read
 mkdir ~/bin
 cd ~/bin
 echo `curl -L https://raw.githubusercontent.com/viktor-chiarcos/Programms/refs/heads/main/git-send.sh` > git-send
