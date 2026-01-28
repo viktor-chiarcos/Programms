7cb77245643c89d5a83411d62472dc521112c9e9
VIKTOR CHIARCOS <viktor.chiarcos@gmail.com>
2026-01-28 12:04:42 +0000

    Add user prompt and fix directory check in install.sh
    Prompt user before installation and check for bin directory.

--- vpackage/git-send/install.sh
+++ vpackage/git-send/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ !e $HOME/bin ]
-then mkdir ~/bin
+printf "Klicke ENTER um die Installation zu starten oder drücke Strg/Control+C zum abbrechen"
+read
+if [ -e $HOME/bin ]
+then cd ~/bin
+else mkdir ~/bin
+  cd ~/bin
 fi
-cd ~/bin
 
 echo `curl -L https://raw.githubusercontent.com/viktor-chiarcos/Programms/refs/heads/main/git-send.sh` > git-send
 chmod a+x git-send
